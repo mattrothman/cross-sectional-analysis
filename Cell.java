c4cddcccf4088e232bd7e24c28971bbe79fa3330
mattrothman <60554019+mattrothman@users.noreply.github.com>
2020-02-01 12:45:49 -0800

    To do is in cell class

--- Cell.java
+++ Cell.java
@@ -1,1 +1,9 @@
-
+//To do
+//Create wrapper class "CellCollection" or something like that, holds a hashmap of cells, where key is the number label, value is the cell object
+//Create cell class where each cell has global variables xpoints, ypoints, 
+//and has methods inside(x, y)
+//Create Traverser class that traverses image, from left to right, searching for cell
+//Traverser class has globarl variables lastx, lasty that holds the last point that was checked for a cell,
+//click_distance, and has method click() which calls autooutline after checking whether new location is inside()
+//an already existing cell, and checkdiameter() which checks is a cell is large enough, addcell(), adds to cellcollection
+//Preprocessing class makes the image grayscale or black and white
