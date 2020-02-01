eca7d8b042f8c11534a5d1cd00786471c54c9168
thaliabarrmalec <thaliabarrmalec@gmail.com>
2020-02-01 13:28:32 -0800

    Update Traverser.java

--- Traverser.java
+++ Traverser.java
@@ -1,1 +1,5 @@
-
+//Create Traverser class that traverses image, from left to right, searching for cell
+//Traverser class has globarl variables lastx, lasty that holds the last point that was checked for a cell,
+//click_distance, and has method click() which calls autooutline after checking whether new location is inside()
+//an already existing cell, and checkdiameter() which checks is a cell is large enough, addcell(), adds to cellcollection
+//Preprocessing class makes the image grayscale or black and white
