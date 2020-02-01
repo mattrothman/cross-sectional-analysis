f14b23cfe2472815f4bcced174366451cd1e2ba1
thaliabarrmalec <thaliabarrmalec@gmail.com>
2020-02-01 14:05:50 -0800

    Update PreProcessor.java

--- PreProcessor.java
+++ PreProcessor.java
@@ -1,2 +1,15 @@
-// Variables
-//    
+/**
+Variables:
+    image //cell image
+    
+Methods:
+
+    makeBlackAndWhite //uses functionality built into ImageJ to convert the image to black and white
+    
+    adjustContrast //uses functionality built into ImageJ to convert the image to adjust the contrast. If you decrease
+                     minimum and maximum and increase contrast you get a much crisper image
+        link to ImageJ code in github: https://github.com/imagej/imagej1/blob/c1045944023a30da378d0e8996c6efb44bd9d9b7/ij/plugin/frame/ContrastAdjuster.java
+    
+    
+    
+*/
