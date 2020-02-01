46d3d91dc72fa9a32ded4672c4e5b56e7a0c3b94
thaliabarrmalec <thaliabarrmalec@gmail.com>
2020-02-01 14:20:04 -0800

    Update Record.java

--- Record.java
+++ Record.java
@@ -1,3 +1,13 @@
-//**
+/**
+Holds all the Cell objects and prints a report with the cells and areas.
+*/
 
+/**
+Variables:
+    cells //holds an array of Cell objects? Matt I think you were talking about using a hashmap. Feel free to change this
+Methods:
+
+    addCell //adds a Cell object to the cells variable
+    
+    printData //creates file and writes each cell number with it's corresponding area
 */
