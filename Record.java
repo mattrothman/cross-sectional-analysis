63bd0b22b498c24324e58060c3a63a38d4f2acdd
mattrothman <60554019+mattrothman@users.noreply.github.com>
2020-02-03 14:44:29 -0800

    Suggest cells be an arraylist

--- Record.java
+++ Record.java
@@ -5,6 +5,7 @@ Holds all the Cell objects and prints a report with the cells and areas.
 /**
 Variables:
     cells //holds an array of Cell objects? Matt I think you were talking about using a hashmap. Feel free to change this
+          //After thinking about the issue of changing the cell numberings, i think an arrayList might be our best bet, to begin with
 Methods:
 
     addCell //adds a Cell object to the cells variable
