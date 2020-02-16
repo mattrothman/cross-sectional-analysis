bc156581ce7ab250869be29e6da72d52f27de059
Nalin Richardson <richardson.nalin@gmail.com>
2020-02-16 11:57:11 -0800

    Started PreProcessing

--- PreProcessor.java
+++ PreProcessor.java
@@ -1,3 +1,36 @@
+import ij.process.*;
+
+class Preprocessor {
+    ImagePlus imp;
+    ImageProcessor ip
+
+    public Preprocessor(ImagePlus imp, ImageProcessor ip) {
+        this.imp = imp;
+        this.ip = ip;
+    }
+
+    /** 
+     Several different things we can do to our image
+        - Sharpen: shapens our image
+        - Smooth: smooths lines, could potentially fill gaps
+        - Find edges: finds all the cell edges in our image
+        - Enhance contrast: turning up saturation can provide a clearer image, I wouldn't go higher than 20%
+        - Make binary: turns all colors either black or white, can help wand tool find edges of the cells but 
+            may leave more gaps than without
+        - Noise - Despeckle: can help fade noise within cells
+     */
+
+    /**
+     * Process 
+     */
+    public void process() {
+        ip.setSnapshotCopyMode(true);
+	 	ip.findEdges();
+        ip.setSnapshotCopyMode(false);
+    }
+
+}
+
 /**
 Preprocesses image to sharpen the contrast and make it easier to distinguish cells.
 Things to consider: if the image is already preprocessed by the user danger in overprocessing? --> processing thickens cell 
